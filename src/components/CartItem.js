dbf37ecb86e6450d3d4217b567863fa0dae3e31f
Sarah <sarah.dulat@gmail.com>
2020-11-29 00:06:43 -0500

    Styling cart items

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -14,11 +14,11 @@ const CartItem = ({ item, onRemoveFromCart }) => {
         alt={item.name}
       />
       <div className="cart-item__details">
-        <h4 className="cart-tem__details-name">{item.name}</h4>
+        <h4 className="cart-item__details-name">{item.name}</h4>
         <div className="cart-item__details-qty">
           <p>{item.quantity}</p>
         </div>
-        <div className="cart-tem__details-price">
+        <div className="cart-item__details-price">
           {item.line_total.formatted_with_symbol}
         </div>
       </div>
