0ef5a49dc1eebe3c2ad6da1acd776c2c58594399
Sarah <sarah.dulat@gmail.com>
2020-11-29 11:18:39 -0500

    Styling cart item remove

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,5 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faTrash);
 
 const CartItem = ({ item, onRemoveFromCart }) => {
   const handleRemoveFromCart = () => {
@@ -16,19 +21,19 @@ const CartItem = ({ item, onRemoveFromCart }) => {
       <div className="cart-item__details">
         <h4 className="cart-item__details-name">{item.name}</h4>
         <div className="cart-item__details-qty">
-          <p>{item.quantity}</p>
-        </div>
-        <div className="cart-item__details-price">
-          {item.line_total.formatted_with_symbol}
+          <p>x {item.quantity}</p>
         </div>
       </div>
-      <button
-        type="button"
-        className="cart-item__remove"
-        onClick={handleRemoveFromCart}
-      >
-        Remove
-      </button>
+      <div className="cart-item__details-price">
+        {item.line_total.formatted_with_symbol}
+        <button
+          type="button"
+          className="cart-item__remove"
+          onClick={handleRemoveFromCart}
+        >
+          <FontAwesomeIcon size="1x" icon="trash" color="#292B83" />
+        </button>
+      </div>
     </div>
   );
 };
